f2a76641779397eca01ca3173e1984115d59c91f
ziadashraf0 <ziadshreet@gmail.com>
2024-12-12 14:24:44 +0200

    connecting optimizely

---
+++ src/api/BlockApi.ts
@@ -0,0 +1,51 @@
+export async function fetchMainContent() {
+  const url = process.env.GRAPHQL_API_URL as string;
+
+  if (!url) {
+    throw new Error(
+      "GRAPHQL_API_URL is not defined in the environment variables"
+    );
+  }
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+          query {
+            startPage {
+              mainContent {
+                __typename
+                ... on HeroBannerDto {
+                  buttonText
+                  identifier
+                  desktopImage
+                  mobileImage
+                  name
+                  texts {
+                    fontSize
+                    text
+                    color
+                  }            
+                }
+                ... on TeaserBlockDto {
+                  text
+                }
+              }
+            }
+          }
+        `,
+    }),
+    next: { revalidate: 10 },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data: ${response.statusText}`);
+  }
+
+  const { data } = await response.json();
+
+  return data.startPage.mainContent;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { fetchMainContent } from "@/api/BlockApi";
 
-export default function Home() {
+export default async function Home() {
+  const mainContent = await fetchMainContent();
+  const heroBannerContent = mainContent.find(
+    (item: { __typename: string }) => item.__typename === "HeroBannerDto"
+  );
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div>
+          <h1>{heroBannerContent.name}</h1>
+          <h1>{heroBannerContent.buttonText}</h1>
         </div>
       </main>
       <footer className={styles.footer}>
